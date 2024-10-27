112ec060c0f9b628dd69e93e8354bab1f0602705
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2024-10-27 05:24:12 -0400

    backend: added the no cache flag

--- lucid-backend/video.py
+++ lucid-backend/video.py
@@ -213,7 +213,7 @@ class CustomVoiceoverScene(VoiceoverScene):
     
     try:
         result = subprocess.run(
-            ['manim', '-ql', '-o', output_file, manim_code_filename, '--renderer=opengl', '--write_to_movie'],
+            ['manim', '-ql', '-o', output_file, manim_code_filename, '--renderer=opengl', '--write_to_movie', '--disable_caching'],
             check=True,
             capture_output=True,
             text=True
